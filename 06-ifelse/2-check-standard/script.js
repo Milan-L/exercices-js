aaeb2d8c2a6ba159d60dadddd197dda512b0154d
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-13 13:13:36 +0200

    06-2 check standard

--- 06-ifelse/2-check-standard/script.js
+++ 06-ifelse/2-check-standard/script.js
@@ -1,4 +1,9 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+
+
+let value = prompt(' Quel est le nom "officiel" de JavaScript?', "");
+
+if (value == 'ECMAScript') {
+alert('Bonne réponse!');
+      } else {
+alert("Ne sait pas ?");
+    }
