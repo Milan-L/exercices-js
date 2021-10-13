3dc5a2dec9d15320ea379e76bcecd9e09d9b4e6f
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-13 13:07:24 +0200

    06-1 if zero string

--- 06-ifelse/1-if-zero-string/script.js
+++ 06-ifelse/1-if-zero-string/script.js
@@ -1,4 +1,5 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+if ("0") {
+      alert( 'Hello' );
+    }
+    //Est-ce que alert sera affiché ? 
+    // L'alert va afficher hello
