2f122fe7b0fdd704f2f7bc5286c363f69c9a68ad
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-13 12:42:50 +0200

    04-3 primitive

--- 04-operators/3-primitive-conversions-questions/script.js
+++ 04-operators/3-primitive-conversions-questions/script.js
@@ -1,4 +1,14 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+"" + 1 + 0 // égal "10"
+"" - 1 + 0 // -1
+true + false // 4
+6 / "3" // 2 
+"2" * "3" // 6
+4 + 5 + "px" // "9x"
+"$" + 4 + 5 // "§45"
+"4" - 2 // 2
+"4px" - 2 // Nan
+"  -9  " + 5 // " -9 5"
+"  -9  " - 5 // -14
+null + 1 // 0
+undefined + 1 // nan
+" \t \n" - 2 // je ne saus pas 
