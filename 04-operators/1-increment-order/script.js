2ed70afb85e2092ae093ea30babc56b3ddf56c37
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-13 12:33:27 +0200

    04-1 increment order

--- 04-operators/1-increment-order/script.js
+++ 04-operators/1-increment-order/script.js
@@ -1,4 +1,10 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let a = 1, b = 1;
+
+let c = ++a;
+let d = b++;
+
+// Voici les résulats de chaque variable
+// a=2
+//b=2
+//c=2
+//d=1
