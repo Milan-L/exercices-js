37a919a37d60150619e737a666dd426cb2df3ef8
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 09:20:24 +0200

    07-2 alert or

--- 07-logical-operators/2-alert-or/script.js
+++ 07-logical-operators/2-alert-or/script.js
@@ -1,4 +1,2 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+alert( alert(1) || 2 || alert(3) ); 
+// Va envoyer 1 ensuite 2 et se ferme
