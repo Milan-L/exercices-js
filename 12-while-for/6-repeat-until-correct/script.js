e5c2f66757bf8c7d872dc31b49a2fd14acea1022
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 14:18:32 +0200

    12-6 repeat until correct

--- 12-while-for/6-repeat-until-correct/script.js
+++ 12-while-for/6-repeat-until-correct/script.js
@@ -1,4 +1,6 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let num;
+
+do {
+  num = prompt("Entrer un nombre supérieur à 100", 0);
+} while (num <= 100 && num);
+
