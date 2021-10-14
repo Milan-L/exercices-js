ea85224814a7609f0a62c5a9e6357b4bca7f5c70
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 13:47:42 +0200

    12-3 which value for

--- 12-while-for/3-which-value-for/script.js
+++ 12-while-for/3-which-value-for/script.js
@@ -1,4 +1,12 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+/*Pour chaque boucle, notez les valeurs qui vont s'afficher. Ensuite, comparez avec la réponse.
+
+Les deux boucles alert les mêmes valeurs ou pas ?
+
+La forme postfix :
+
+/*for (let i = 0; i < 5; i++) alert( i ); 
+//for (let i = 0; i < 5; ++i) alert( i );
+
+/* Résultat : premier boucle 0,1,2,3,4,
+Seconde boucle: 0,1,2,3,4,5 
+le postfix et préfix ne change pas la valeur alert
