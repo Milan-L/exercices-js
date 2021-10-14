7bde8e686542340ae4906ae078284e83a2265f2c
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 09:58:50 +0200

    07-9 check login

--- 07-logical-operators/9-check-login/script.js
+++ 07-logical-operators/9-check-login/script.js
@@ -1,4 +1,19 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+let userName = prompt("Who's there?", '');
+
+if (userName === 'Admin') {
+
+  let pass = prompt('Password?', '');
+
+  if (pass === 'TheMaster') {
+    alert( 'welcome!'  );
+  } else if (pass === '' || pass === null) {
+    alert( 'Canceled' );
+  } else {
+    alert( 'Wrong password' );
+  }
+
+} else if (userName === '' || userName === null) {
+  alert( 'Canceled' );
+} else {
+  alert( "I don't know you" );
+}
