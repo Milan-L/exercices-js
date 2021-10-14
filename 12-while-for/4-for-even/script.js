4540682cc72ca60e212338a06f5bea0af5fd39c0
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 14:02:48 +0200

    12-4

--- 12-while-for/4-for-even/script.js
+++ 12-while-for/4-for-even/script.js
@@ -1,4 +1,5 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+for (let i = 2; i <= 10; i++) {
+ if (i % 2 == 0) {
+ alert( i );
+ }
+}
