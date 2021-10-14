6ed8e7afc1db20988183a7fd6c1322ee6b2ef52b
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 11:39:32 +0200

    10-3 min

--- 10-function-basics/3-min/script.js
+++ 10-function-basics/3-min/script.js
@@ -1,4 +1,7 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+function min(a, b) {
+   if (a < b) {
+      return a;
+      } else {
+      return b;
+      }
+}
