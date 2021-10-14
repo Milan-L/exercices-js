f62a5a8e21b3d10b80e62fba6963675b2c8d2be9
Maxime Larrouquere <larrouquerem@gmail.com>
2021-10-14 11:49:59 +0200

    11-1 arrow function basics

--- 11-arrow-functions-basics/1-rewrite-arrow/script.js
+++ 11-arrow-functions-basics/1-rewrite-arrow/script.js
@@ -1,4 +1,11 @@
-/*  Votre code Javascript vient ici:
-      - Vous pourrez visualiser le résultat en ouvrant index.html dans votre navigateur.
-      - N'oubliez pas de sauvegarder pour afficher les changements.
-*/
+function ask(question, yes, no) {
+  if (confirm(question)) yes();
+  else no();
+
+}
+    
+ask(
+    "Do you agree?",
+     () => alert("You agreed."),
+     () => alert("You canceled the execution.")
+);
